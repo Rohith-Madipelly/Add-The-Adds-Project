6f1b254c065ed3227d1dfcf572f615c3230d3f5b
Rohith Madipelly <madipellyrohith@gmail.com>
2024-04-03 11:31:42 +0530

    push day2

--- src/Enviornment.jsx
+++ src/Enviornment.jsx
@@ -1,3 +1,3 @@
 // export const BASE_URL="http://localhost:8001"
-export const BASE_URL="https://food-admin.satyasadhna.com"
-export const GUEST_URL="http://localhost:8001"
+export const BASE_URL="http://addtheadds.satyasadhna.com"
+export const GUEST_URL="http://addtheadds.satyasadhna.com"
